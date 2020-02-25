0be4346ffd0dcf86d7c709ee1f2c068e0d7e18fa
Steven Jackson <stevenpjackson0@gmail.com>
2020-02-25 11:42:16 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Upload an Image</title>
+        <style>
+            .error {
+                font-weight: bold;
+                color: #C00;
+            }
+        </style>
+    </head>
+    <body>
+        
+    </body>
+</html>
